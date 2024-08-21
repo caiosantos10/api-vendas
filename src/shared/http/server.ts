c54c8c06a38031eed8d3e41d314bcf87e0c96c8e
Caio Santos <caiosant10@gmail.com>
2024-08-20 22:16:49 -0300

    Middleware para condicionamento de erros entre tratados e erros não tratados (externos)

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,8 @@
-import express from 'express';
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import AppError from '@shared/errors/AppError';
 
 const PORT = 3333;
 const app = express();
@@ -9,6 +11,27 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(
+    (
+        error: Error,
+        request: Request,
+        response: Response,
+        next: NextFunction
+    ) => {
+        if (error instanceof AppError) {
+            return response.status(error.statusCode).json({
+                status: 'error',
+                message: error.message,
+            });
+        }
+
+        return response.status(500).json({
+            status: 'error',
+            message: 'Internal server error',
+        });
+    }
+);
 
 app.listen(PORT, () => {
     console.log(`Server starterd on port ${PORT}`);
