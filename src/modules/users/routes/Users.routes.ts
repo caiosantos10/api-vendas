69287c941e419d03a73cb0fd6ea532a0eb6c770c
Caio Santos <caiosant10@gmail.com>
2024-12-02 22:06:13 -0300

    Criando Controller e Routes para User

---
+++ src/modules/users/routes/Users.routes.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import UsersController from "../controllers/UsersController";
+
+const usersRouter = Router();
+const usersController = new UsersController();
+
+usersRouter.get('/', usersController.index);
