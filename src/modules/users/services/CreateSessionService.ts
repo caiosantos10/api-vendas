a84a769574b452c14bd152acd7a0771f50bc9b8b
Caio Santos <caiosant10@gmail.com>
2024-12-12 23:23:33 -0300

    Retornando token no serviço de criação de sessão

--- src/modules/users/services/CreateSessionService.ts
+++ src/modules/users/services/CreateSessionService.ts
@@ -3,14 +3,19 @@ import { getCustomRepository } from "typeorm";
 import UserRepository from "../typeorm/repositories/UserRepository";
 import User from "../typeorm/entities/User";
 import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
 interface IRequest {
   email: string;
   password: string;
 }
+interface IResponse {
+  user: User;
+  token: string;
+}
 
 export default class CreateSessionService {
-  public async execute({ email, password }: IRequest): Promise<User> {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
     const usersRepository = getCustomRepository(UserRepository);
     const user = await usersRepository.findByEmail(email);
 
@@ -24,6 +29,14 @@ export default class CreateSessionService {
       throw new AppError('Incorrect email/password combination.');
     }
 
-    return user;
+    const token = sign({}, '155e7bf674b6b8eef41afce949af5ee9', {
+      subject: user.id,
+      expiresIn: '1d'
+    });
+
+    return {
+      user,
+      token,
+    };
   }
 }
