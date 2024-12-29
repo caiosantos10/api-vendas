e0f39b5ab4095378810e2286c54433dd8ee44c75
Caio Santos <caiosant10@gmail.com>
2024-12-29 16:31:41 -0300

    renomeando interface ITokenPayload

--- src/shared/http/middlewares/isAuthenticated.ts
+++ src/shared/http/middlewares/isAuthenticated.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from "express";
 import authConfig from "@config/auth";
 import { verify } from "jsonwebtoken";
 
-interface TokenPayload {
+interface ITokenPayload {
   iat: number;
   exp: number;
   sub: string;
@@ -20,7 +20,7 @@ export default function isAuthenticated(request: Request, response: Response, ne
 
   try {
     const decodedToken = verify(token, authConfig.jwt.secret);
-    const { sub } = decodedToken as TokenPayload;
+    const { sub } = decodedToken as ITokenPayload;
     request.user = {
       id: sub
     };
