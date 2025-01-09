87f535750c8cc2dc907a7566656ff5023ae12b90
Caio Santos <caiosant10@gmail.com>
2025-01-09 01:23:39 -0300

    Template HandleBars para email

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -17,13 +17,23 @@ export default class SendForgotPasswordEmailService {
 
     if (!user) throw new AppError('User does not exists');
 
-    const token = await userTokenRepository.generate(user.id);
+    const { token } = await userTokenRepository.generate(user.id);
 
     // console.log(token);
 
     await EtherealMail.sendMail({
-      to: email,
-      body: `Solicitação de redefinição de senha recebida: ${token?.token}`,
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[API Vendas] Recuperação de senha',
+      templateData: {
+        template: `Olá {{name}}: {{token}}`,
+        variables: {
+          name: user?.name,
+          token,
+        }
+      }
     });
   }
 }
